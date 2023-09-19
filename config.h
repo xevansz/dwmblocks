e35c39a5c92548d8c59d7e545d520b37e743af5f
Zen <76154112+xevansz@users.noreply.github.com>
2023-09-19 19:55:52 +0530

    Add files via upload

---
+++ config.h
@@ -0,0 +1,9 @@
+static const Block blocks[] = {
+
+/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ 
+  {"",  "greet", 5,  4},
+  {"",  "volume", 20, 4}
+};
+
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
